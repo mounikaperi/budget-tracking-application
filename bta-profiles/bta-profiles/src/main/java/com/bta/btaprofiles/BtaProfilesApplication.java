223db971d3c96f1fa90b417734488c1b23be6b45
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-05-03 16:08:07 +0530

    Initialized bta-profiles spring microservice

---
+++ bta-profiles/bta-profiles/src/main/java/com/bta/btaprofiles/BtaProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.bta.btaprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtaProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtaProfilesApplication.class, args);
+	}
+
+}
