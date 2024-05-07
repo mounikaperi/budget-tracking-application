6af62af5edadafcc94a295c4d2d553229be8cdef
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-05-07 16:27:14 +0530

    Working on transaction controller

---
+++ bta-txns/src/main/java/com/bta/btatxns/controllers/TransactionController.java
@@ -0,0 +1,51 @@
+package com.bta.btatxns.controllers;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bta.btatxns.entities.Transaction;
+import com.bta.btatxns.exceptions.AccountHolderException;
+import com.bta.btatxns.exceptions.TransactionException;
+import com.bta.btatxns.services.TransactionService;
+
+@RestController
+@RequestMapping("/")
+public class TransactionController {
+	
+	@Autowired
+	private TransactionService transactionService;
+	
+	@GetMapping("/{accountHolderId/balance")
+	public ResponseEntity<Double> getBalance(@PathVariable("accountHolderId")Long accountHolderId) throws AccountHolderException {
+		return ResponseEntity.ok(transactionService.getBalance(accountHolderId));
+	}
+	
+	@GetMapping("/{accountHolderId}/{start}/{end}")
+	public ResponseEntity<List<Transaction>> getTransactions(
+			@PathVariable("accountHolderId")Long accountHolderId,
+			@PathVariable("start") @DateTimeFormat(iso=ISO.DATE)LocalDate start,
+			@PathVariable("end") @DateTimeFormat(iso=ISO.DATE)LocalDate end
+			) throws AccountHolderException {
+		return ResponseEntity.ok(transactionService.getPeriodicTransactionsForAccountHolderId(accountHolderId, start, end));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Transaction> addTransaction(@RequestBody @Valid Transaction transaction, BindingResult result) throws TransactionException {
+		return ;
+	}
+	
+}
