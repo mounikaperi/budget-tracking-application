fab76a156004c9d863b7a5be4099647034a525b6
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-05-09 18:11:31 +0530

    Started working on statement service

---
+++ bta-statement/src/main/java/com/bta/btastatement/BtaStatementApplication.java
@@ -0,0 +1,13 @@
+package com.bta.btastatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtaStatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtaStatementApplication.class, args);
+	}
+
+}
---
+++ bta-statement/src/test/java/com/bta/btastatement/BtaStatementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bta.btastatement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtaStatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
